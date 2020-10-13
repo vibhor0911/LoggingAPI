29fe0673ac51ad4dda5af352e08ec73063a601bd
Vibhor Gupta <vibgupta@microsoft.com>
2020-10-13 16:41:24 +0530

    add user logging function

---
+++ MVC-Nlog/Models/TokenModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggingAPI.Models
+{
+    public class TokenModel
+    {
+        public string token { get; set; }
+    }
+}
